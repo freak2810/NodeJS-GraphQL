60ff922b1e7edb48434b24a057e8b055716cb798
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-01-02 14:21:09 +0530

    end of chap 30 testing using mocha

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const graphqlHttp = require('express-graphql');
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
-const { clearImage } = require('./util/file');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -18,7 +15,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+    cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
 
@@ -48,50 +45,11 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
   next();
 });
 
-app.use(auth);
-
-app.put('/post-image', (req, res, next) => {
-
-  if (!req.isAuth) {
-    throw new Error('Not Authenticated');
-  }
-
-  if (!req.file) {
-    return res.status(200).json({ message: 'No file Provided' });
-  }
-
-  if (req.body.oldPath) {
-    clearImage(req.body.oldPath);
-  }
-
-  console.log(req.file.path);
-  return res.status(200).json({ message: 'File Stored', filePath: req.file.path.replace(/\\/g, "/") });
-});
-
-
-app.use(
-  '/graphql',
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || 'An error occurred.';
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    }
-  })
-);
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -113,4 +71,3 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 const User = require('../models/user');
 
 exports.signup = async (req, res, next) => {
@@ -60,11 +61,13 @@ exports.login = async (req, res, next) => {
       { expiresIn: '1h' }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
 };
 
